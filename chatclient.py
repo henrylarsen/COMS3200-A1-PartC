784d43a90095f588cd17d72ba67c9bd7e8be0356
henrylarsen <76803334+henrylarsen@users.noreply.github.com>
2023-04-07 13:39:19 +1000

    refactor for OOP

--- chatclient.py
+++ chatclient.py
@@ -1,31 +1,37 @@
 import socket
 import sys
 
-SERVER_HOST = socket.gethostname()
-client_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Client:
+    def __init__(self, channel_port, username):
+        self.channel_port = channel_port
+        self.username = username
+        self.host = socket.gethostname()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if len(sys.argv) != 3:
-    exit(1)
+    def connect_channel(self):
+        self.socket.connect((self.host, self.channel_port))
+        self.socket.send(username.encode())
+        # Receive and print welcome message
+        welcome_message = self.socket.recv(1024).decode()
+        print(welcome_message)
 
-SERVER_PORT = int(sys.argv[1])
-username = sys.argv[2]
 
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        exit(1)
 
-client_skt.connect((SERVER_HOST, SERVER_PORT))
-# send username to server
-client_skt.send(username.encode())
-# Receive and print welcome message
-welcome_message = client_skt.recv(1024).decode()
-print(welcome_message)
+    SERVER_PORT = int(sys.argv[1])
+    username = sys.argv[2]
 
-# message = input('Input: ')
-# client_skt.send(message.encode())
+    client = Client(SERVER_PORT, username)
+    client.connect_channel()
 
-response = client_skt.recv(1024).decode()
-
-print('Received from server: ', response)
+    # while True:
+    #     message = input()
+    #     client.send(message)
 
 # if message == 'quit':
 #     client_skt.close()
 
-# python3 chatclient.py 12351 tank
+# python3 chatclient.py 12351 user0
+# python3 chatclient.py 12352 user1
--- chatserver.py
+++ chatserver.py
@@ -1,53 +1,96 @@
 import socket
 import threading
 import sys
-
-SERVER_HOST = socket.gethostname()
-
-
-def client_handler(conn, addr, chan_name):
-    print(f'New client connection: {addr}')
-    username = conn.recv(1024).decode()
-    conn.send(f'[Server message (<time>)] welcome to the {chan_name} channel, {username}.'.encode())
-    # while True
+from datetime import datetime
 
 
-def initiate_chat_channel(channel_name, channel_port, channel_cap):
-    # Create the server socket
-    server_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Bind the socket to local host
-    server_skt.bind((SERVER_HOST, channel_port))
-    # Listen for incoming connections
-    server_skt.listen()
-    print('The server is listening for connections')
-    clients = []
-
-    while True:
-        conn, addr = server_skt.accept()
-
-        print(f'Accepted new connection from {addr}')
-        client_thread = threading.Thread(target=client_handler, args=(conn, addr, channel_name))
-        client_thread.start()
+class Server:
+    def __init__(self, channels):
+        self.channels = channels
 
 
-if len(sys.argv) != 2:
-    exit(1)
+    def start(self):
+        # Create the server socket
+        # server_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Bind the socket to local host
+        # server_skt.bind((self.host, channel_port))
 
-config_file = sys.argv[1]
+        for channel in self.channels:
+            channel_thread = threading.Thread(target=channel.start())
+            channel_thread.start()
 
-# Gets channel info from configfile, creates channels from it.
-for line in open(config_file):
-    if line.startswith('channel'):
-        fields = line.strip().split()
-        if len(fields) != 4:
-            print(f'Incorrect formatting in config file.')
+        # Listen for incoming connections
+        # server_skt.listen()
+        print('The server is listening for connections')
 
-        channel_name = fields[1]
-        channel_port = int(fields[2])
-        channel_cap = int(fields[3])
+        # while True:
+        #     conn, addr = server_skt.accept()
+        #
+        #     print(f'Accepted new connection from {addr}')
+        #     client_thread = threading.Thread(target=self.client_handler, args=(conn, addr, channel_name))
+        #     client_thread.start()
 
-        server = threading.Thread(target=initiate_chat_channel, args=(channel_name, channel_port, channel_cap))
-        server.start()
+
+    # def client_handler(self, conn, addr, channel_port):
+    #     print(f'New client connection: {addr}')
+    #     username = conn.recv(1024).decode()
+    #     time = datetime.now().strftime("%H:%M:%S")
+    #     conn.send(f'[Server message ({time})] welcome to the {chan_name} channel, {username}.'.encode())
+    #     # while True
+
+
+class Channel:
+    def __init__(self, name, port, capacity):
+        self.port = port
+        self.name = name
+        self.capacity = capacity
+        self.clients = []
+        self.queue = []
+        self.host = socket.gethostname()
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Bind the socket to local host
+        self.socket.bind((self.host, self.port))
+        self.socket.listen()
+
+    def start(self):
+        while True:
+            conn, addr = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            client_thread.start()
+
+    def handle_client(self, conn, addr):
+        print(f'New client connection: {addr}')
+        username = conn.recv(1024).decode()
+        time = datetime.now().strftime("%H:%M:%S")
+        conn.send(f'[Server message ({time})] welcome to the {self.name} channel, {username}.'.encode())
+        # while True
+
+
+if __name__ == '__main__':
+    # If no config file, exit
+    if len(sys.argv) != 2:
+        exit(1)
+
+    config_file = sys.argv[1]
+
+    channel_list = []
+
+    # Gets channel info from configfile, creates channels from it.
+    for line in open(config_file):
+        if line.startswith('channel'):
+            fields = line.strip().split()
+            if len(fields) != 4:
+                print(f'Incorrect formatting in config file.')
+
+            channel_name = fields[1]
+            channel_port = int(fields[2])
+            channel_cap = int(fields[3])
+
+            channel_list.append(Channel(channel_name, channel_port, channel_cap))
+
+    server = Server(channel_list)
+    server.start()
 
 
 # python3 chatserver.py configfile
